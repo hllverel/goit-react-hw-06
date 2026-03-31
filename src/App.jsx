77c36840acedc5d455d620af7d4537119b55bd8f
hllverel <hllozdemir@live.com>
2026-03-31 14:30:29 +0300

    created modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
-
+import { useState, useEffect } from 'react'
+import initialContacts from './contacts.json'
+import ContactList from './components/ContactList/ContactList'
+import SearchBox from './components/SearchBox/SearchBox';
+import ContactForm from './components/ContactForm/ContactForm';
 
 function App() {
-  return (
-    <div>
-    </div>
+  const [contacts, setContacts] = useState(() => {
+    return localStorage.getItem("saved-contacts") ? JSON.parse(localStorage.getItem("saved-contacts")) : initialContacts;
+  });
+
+  const [filter, setFilter] = useState('');
+
+  const addContacts = (newContact) => {
+    setContacts((prevContacts) => {
+      return [...prevContacts, newContact];
+    });
+  };
+
+    useEffect(() => {
+    window.localStorage.setItem("saved-contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
+  const deleteContact = (contactId) => {
+    setContacts((prevContacts) => {
+      return prevContacts.filter((contact) => contact.id !== contactId);
+    });
+  };
+
+  const visibleContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
   );
+
+  return (
+    <div className='phonebook'>
+      <h1>Phonebook</h1>
+      <ContactForm onAdd={addContacts}/>
+      <SearchBox value={filter} onFilter={setFilter} />
+      <ContactList contacts={visibleContacts} onDelete={deleteContact}/>
+    </div>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,23 @@
+import css from './Contact.module.css'
+
+const Contact = ({ contact, onDelete }) => {
+    const { name, number } = contact;
+    
+    return (
+        <div className={css.contactcard}>
+            <div className={css.contactinfo}>
+                <div>
+                    <svg><use href="/src/assets/symbol-defs.svg#icon-user"></use></svg>
+                    <p>{name}</p>
+                </div>
+                <div>
+                    <svg><use href="/src/assets/symbol-defs.svg#icon-phone"></use></svg>
+                    <p>{number}</p>
+                </div>
+            </div>
+            <button className={css.deletebutton} onClick={() => onDelete(contact.id)}>Delete</button>
+        </div>
+    )
+}
+
+export default Contact
