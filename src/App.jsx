d4de660b65fd89487c4e8f044fc8ab6bb81793ce
hllverel <hllozdemir@live.com>
2026-04-05 01:46:37 +0300

    minor updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { addContact, deleteContact } from './redux/contactsSlice';
 import { setFilter } from './redux/filtersSlice';
 import ContactList from './components/ContactList/ContactList'
 import SearchBox from './components/SearchBox/SearchBox';
@@ -7,43 +6,18 @@ import ContactForm from './components/ContactForm/ContactForm';
 
 function App() {
   const dispatch = useDispatch();
+  const filter = useSelector(state => state.filters.status);
 
-  const contacts = useSelector(state => state.contacts.items);
-  const filter = useSelector(state => state.filters.value);
-
-
-  const [contacts, setContacts] = useState(() => {
-    return localStorage.getItem("saved-contacts") ? JSON.parse(localStorage.getItem("saved-contacts")) : initialContacts;
-  });
-
-  const [filter, setFilter] = useState('');
-
-  const addContacts = (newContact) => {
-    setContacts((prevContacts) => {
-      return [...prevContacts, newContact];
-    });
+  const handleFilterChange = (filterValue) => {
+    dispatch(changeFilter(filterValue));
   };
-
-    useEffect(() => {
-    window.localStorage.setItem("saved-contacts", JSON.stringify(contacts));
-  }, [contacts]);
-
-  const deleteContact = (contactId) => {
-    setContacts((prevContacts) => {
-      return prevContacts.filter((contact) => contact.id !== contactId);
-    });
-  };
-
-  const visibleContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
 
   return (
     <div className='phonebook'>
       <h1>Phonebook</h1>
-      <ContactForm onAdd={addContacts}/>
-      <SearchBox value={filter} onFilter={setFilter} />
-      <ContactList contacts={visibleContacts} onDelete={deleteContact}/>
+      <ContactForm />
+      <SearchBox value={filter} onFilter={handleFilterChange} />
+      <ContactList />
     </div>
   )
 }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,15 +2,19 @@ import Contact from '../Contact/Contact'
 import css from './ContactList.module.css'
 import { useSelector } from 'react-redux';
 
-const ContactList = ({ contacts, onDelete }) => {
+const ContactList = () => {
     const contacts = useSelector(state => state.contacts.items);
-    const filter = useSelector(state => state.filters.value);
-    
+    const filter = useSelector(state => state.filters.status);
+
+    const visibleContacts = contacts.filter((contact) =>
+        contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+
     return (
         <ul className={css.contactlist}>
-            {contacts.map((contact) => (
+            {visibleContacts.map((contact) => (
                 <li key={contact.id}>
-                    <Contact contact={contact} onDelete={onDelete}/>
+                    <Contact contact={contact}/>
                 </li>
             ))}
         </ul>
